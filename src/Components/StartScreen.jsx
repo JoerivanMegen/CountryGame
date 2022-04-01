9c705723ca585d62f6bc7629c8c3adb5bbaf9632
JoerivanMegen <joerivanmegen1@gmail.com>
2022-04-02 01:15:07 +0200

    time to go to sleep for today

--- src/Components/StartScreen.jsx
+++ src/Components/StartScreen.jsx
@@ -8,10 +8,9 @@ export default function StartScreen() {
                     {/* insert modal functionality */}
                 </div>
                 <div className="start-title">
-                    Start
+                    <h3>Welcome to Flaggle!</h3>
                 </div>
                 <div className="short-explanation">
-                    <h3>Welcome to Flaggle!</h3>
                     <p> As soon as you click the start button below, the game starts.
                         <br/>
                         To see the rules, you can click the info button in the top right corner.
@@ -19,6 +18,7 @@ export default function StartScreen() {
                         <strong>IMPORTANT:</strong> when you click the start button, you will have limited time to answer the questions. Make sure you are ready!
                     </p>
                 </div>
+                <button>START</button>
             </div>
         </div>
     )
