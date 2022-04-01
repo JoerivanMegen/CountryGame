fc6bb79279535cfba8778e59c69abef0528708fa
JoerivanMegen <joerivanmegen1@gmail.com>
2022-04-02 00:33:13 +0200

    added scss and some basic coloring

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import StartScreen from './Components/StartScreen';
 
 function App() {
   return (
-    <StartScreen>
-    
+    <StartScreen />
   )
+  
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import './index.css';
 
 ReactDOM.render(
   <App/>,
